b1bf71759b54f0c223d321b7a0b257a13335ed60
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-23 10:00:41 +0800

    Delete colour_pages.sh

--- colour_pages.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# From: https://superuser.com/questions/234408/count-bw-color-pages-in-pdf?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
-gs -o - -sDEVICE=inkcov Thesis.pdf | grep -v "^ 0.00000  0.00000  0.00000" | grep "^ " | wc -l
